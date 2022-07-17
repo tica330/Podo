2a29af179ba61dd3e959e3fd3e21758d13860cb1
tica330 <94100925+tica330@users.noreply.github.com>
2022-07-17 14:27:14 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,74 +1,142 @@
 window.addEventListener('load', () => {
 
     // 드롭다운 메뉴
-    const nav=document.querySelector('#nav');
-    const mn=document.querySelector('.nav_mn');
+    const nav = document.querySelector('#nav');
+    const mn = document.querySelector('.nav_mn');
 
-    mn.addEventListener('mouseover',()=>{
-        nav.style.height='240px';
+    mn.addEventListener('mouseover', () => {
+        nav.style.height = '240px';
     });
-    mn.addEventListener('mouseout',()=>{
-        nav.style.height='70px';
+    mn.addEventListener('mouseout', () => {
+        nav.style.height = '70px';
     });
-
 
     // 슬라이드
-    const slide=document.querySelector('.sl_img');
-    const gall=document.getElementsByClassName('sl_img')[0];
-    const btn=document.querySelectorAll('.sl_button li');
-    const prev=document.querySelector('.prev');
-    const next=document.querySelector('.next');
-    let moveSlide=0;
+    const slide = document.querySelector(".slide");
+    let slideWidth = slide.clientWidth;
+    const prevBtn = document.querySelector(".slide_prev_button");
+    const nextBtn = document.querySelector(".slide_next_button");
+    let slideItems = document.querySelectorAll(".slide_item");
+    const maxSlide = slideItems.length;
+    const pagination = document.querySelector(".slide_pagination");
+    let currSlide = 1;
 
-    showSlide();
-
-    function showSlide(){ 
-        for(let b=0; b<btn.length; b++){
-            let n=0;
-            btn[b].index=b;
-
-            //페이저
-            btn[b].addEventListener('click',(e)=>{
-                e.preventDefault();
-                n=e.currentTarget.index;
-                targetX=n*-100;
-                slide.style.left=targetX+'%';
-            
-                for(let s=0; s<btn.length; s++){
-                    if(s==n){
-                        btn[s].classList.add('on');
-                    }else{
-                        btn[s].classList.remove('on');
-                    };
-                };
-            });
-        };
-
-         //이전버튼
-        prev.addEventListener('click',()=>{
-            const lastGall=gall.lastElementChild;
-	        const firstGall=gall.firstElementChild;
-	        const newnode=lastGall.cloneNode(true);
-	        gall.removeChild(lastGall);
-	        gall.insertBefore(newnode,firstGall);
-        });
-        //다음버튼
-        next.addEventListener('click',()=>{
-            const firstGall=gall.firstElementChild;
-	    	const newnode=firstGall.cloneNode(true);
-	    	gall.removeChild(firstGall);
-	    	gall.appendChild(newnode);
-        });
-    };
-    
-    //auto 슬라이드
-    setInterval(()=>{
-        moveSlide++;
-        if(moveSlide===slide.childElementCount){
-            moveSlide=0;
+    //페이지네이션 생성
+    for (let i = 0; i < maxSlide; i++) {
+        if (i === 0) 
+            pagination.innerHTML += `<li class="active"><img src="images/slide_button.png" alt=""></li>`;
+        else 
+            pagination.innerHTML += `<li><img src="images/slide_button.png" alt=""></li>`;
         }
-        slide.style.setProperty('left','-'+moveSlide+'00%');
-    },3000);
+    
+    const paginationItems = document.querySelectorAll(".slide_pagination > li");
 
+    // 무한 슬라이드 start, end 복사
+    const startSlide = slideItems[0];
+    const endSlide = slideItems[slideItems.length - 1];
+    const startElem = document.createElement("div");
+    const endElem = document.createElement("div");
+
+    endSlide.classList.forEach((c) => endElem.classList.add(c));
+    endElem.innerHTML = endSlide.innerHTML;
+
+    startSlide.classList.forEach((c) => startElem.classList.add(c));
+    startElem.innerHTML = startSlide.innerHTML;
+
+    slideItems[0].before(endElem);
+    slideItems[slideItems.length - 1].after(startElem);
+
+    // 슬라이드 값을 변경해주기 위해 전체 선택
+    slideItems = document.querySelectorAll(".slide_item");
+    let offset = slideWidth + currSlide;
+    slideItems.forEach((i) => {
+        i.setAttribute("style", `left: ${ - offset}px`);
+    });
+
+    function nextMove() {
+        currSlide++;
+        if (currSlide <= maxSlide) {
+            const offset = slideWidth * currSlide;
+            slideItems.forEach((i) => {
+                i.setAttribute("style", `left: ${ - offset}px`);
+            });
+            paginationItems.forEach((i) => i.classList.remove("active"));
+            paginationItems[currSlide - 1].classList.add("active");
+        } else {
+            currSlide = 0;
+            let offset = slideWidth * currSlide;
+            slideItems.forEach((i) => {
+                i.setAttribute("style", `transition: ${0}s; left: ${ - offset}px`);
+            });
+            currSlide++;
+            offset = slideWidth * currSlide;
+            setTimeout(() => {
+                slideItems.forEach((i) => {
+                    i.setAttribute("style", `transition: ${ 0.5}s; left: ${ - offset}px`);
+                });
+            }, 0);
+            paginationItems.forEach((i) => i.classList.remove("active"));
+            paginationItems[currSlide - 1].classList.add("active");
+        }
+    }
+
+    function prevMove() {
+        currSlide--;
+        if (currSlide > 0) {
+            const offset = slideWidth * currSlide;
+            slideItems.forEach((i) => {
+                i.setAttribute("style", `left: ${ - offset}px`);
+            });
+            paginationItems.forEach((i) => i.classList.remove("active"));
+            paginationItems[currSlide - 1].classList.add("active");
+        } else {
+            currSlide = maxSlide + 1;
+            let offset = slideWidth * currSlide;
+            slideItems.forEach((i) => {
+                i.setAttribute("style", `transition: ${0}s; left: ${ - offset}px`);
+            });
+            currSlide--;
+            offset = slideWidth * currSlide;
+            setTimeout(() => {
+                slideItems.forEach((i) => {
+                    i.setAttribute("style", `transition: ${ 0.5}s; left: ${ - offset}px`);
+                });
+            }, 0);
+            paginationItems.forEach((i) => i.classList.remove("active"));
+            paginationItems[currSlide - 1].classList.add("active");
+        }
+    }
+
+    nextBtn.addEventListener("click", () => {
+        nextMove();
+    });
+    prevBtn.addEventListener("click", () => {
+        prevMove();
+    });
+
+    // 브라우저 화면사이즈 반응
+    window.addEventListener("resize", () => {
+        slideWidth = slide.clientWidth;
+    });
+
+    //클릭시 해당 슬라이드 이동
+    for (let i = 0; i < maxSlide; i++) {
+        paginationItems[i].addEventListener("click", () => {
+            currSlide = i + 1;
+            const offset = slideWidth * currSlide;
+            slideItems.forEach((i) => {
+                i.setAttribute("style", `left: ${ - offset}px`);
+            });
+            paginationItems.forEach((i) => i.classList.remove("active"));
+            paginationItems[currSlide - 1].classList.add("active");
+        });
+    }
+
+    // 슬라이드 루프
+    let loopInterval = setInterval(() => {
+        nextMove();
+    }, 3000);
+    
+    loopInterval();
 
 });
